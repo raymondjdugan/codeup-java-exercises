22480423191bba6ba6a1bb4b38fa1a5ca0f8cb96
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-02-28 11:13:23 -0600

    created myFavoriteNumber variable and printed it

--- codeup-java-exercises/src/HelloWorld/HelloWorld.java
+++ codeup-java-exercises/src/HelloWorld/HelloWorld.java
@@ -3,5 +3,7 @@ package HelloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
     }
 }
