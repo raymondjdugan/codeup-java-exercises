f28d4979bf11977bdcc2da08c88a11544adfc30e
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-02-28 11:15:29 -0600

    created myString variable and printed it

--- codeup-java-exercises/src/HelloWorld/HelloWorld.java
+++ codeup-java-exercises/src/HelloWorld/HelloWorld.java
@@ -3,7 +3,11 @@ package HelloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
         int myFavoriteNumber = 7;
         System.out.println(myFavoriteNumber);
+
+        String myString = "Raymond Dugan";
+        System.out.println(myString);
     }
 }
