76fdf17a524dd7adf7647a4ea82b70f1b6c1cb6d
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-01 15:22:06 -0600

    finished bob exercise

--- codeup-java-exercises/src/strings/Bob.java
+++ codeup-java-exercises/src/strings/Bob.java
@@ -1,4 +1,29 @@
 package strings;
 
+import java.util.Scanner;
+
 public class Bob {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean activeConversation = true;
+        while (activeConversation) {
+            System.out.println("Bob says: \"What's up\"");
+            String userInput = input.nextLine();
+            if (userInput.endsWith("?")) {
+                System.out.println("Sure");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (userInput.isEmpty()) {
+                System.out.println("Fine. Be that way");
+            } else {
+                System.out.println("Whatever");
+            }
+
+            System.out.println("Do you want to continue the conversation? Y or N");
+            String userAnswer = input.nextLine();
+            if (userAnswer.equalsIgnoreCase("n")) {
+                activeConversation = false;
+            }
+        }
+    }
 }
