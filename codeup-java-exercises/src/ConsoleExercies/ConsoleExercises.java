98a650712b4fde601d1b1d201684b34c7a999520
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-01 12:24:32 -0600

    added ControlFlowExercises

--- codeup-java-exercises/src/ConsoleExercies/ConsoleExercises.java
+++ codeup-java-exercises/src/ConsoleExercies/ConsoleExercises.java
@@ -18,11 +18,35 @@ public class ConsoleExercises {
         System.out.println("Enter an integer");
         int userInput = input.nextInt();
         System.out.format("You entered: %s%n", userInput);
+
         //TODO: Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.
         System.out.println("Enter three words");
         String firstWord = input.next();
         String secondWord = input.next();
         String thirdWord = input.next();
-        System.out.printf("%s %s %s",firstWord,secondWord,thirdWord);
+        System.out.printf("%s %s %s%n",firstWord,secondWord,thirdWord);
+
+        //TODO: Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
+        System.out.println("Enter a sentence");
+        String userSentence = input.nextLine();
+        System.out.println(userSentence);
+
+//        Calculate the perimeter and area of Codeup's classrooms.
+        System.out.println("Enter the length of the room");
+        int length = Integer.parseInt(input.nextLine());
+        System.out.println("Enter the length of the room");
+        int width = Integer.parseInt(input.nextLine());
+        int perimeter = length * width;
+        int area = (length * 2) + (width * 2);
+        System.out.printf("The perimeter of the classroom is: %sft%n", perimeter);
+        System.out.printf("The area of the classroom is: %sft\u00B2%n", area);
+        System.out.println("Enter the length, width and height of a room. I.E. L/W/H");
+
+
+        int myInt = input.nextInt();
+        System.out.println("1 Int? " + input.hasNextInt());
+        input.nextInt();
+        System.out.println("2 Int? " + input.hasNextInt());
+        input.close();
     }
 }
---
+++ codeup-java-exercises/src/ControlFlowExercises/ControlFlowExercises.java
@@ -0,0 +1,17 @@
+package ControlFlowExercises;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 15) {
+//            System.out.println(i);
+            i++;
+        }
+
+        int j = 2;
+        do {
+            System.out.println(j);
+            j += 2;
+        } while (j <= 100);
+    }
+}
