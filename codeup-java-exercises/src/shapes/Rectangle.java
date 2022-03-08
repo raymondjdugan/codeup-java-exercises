fa994c34b67cd34ad38c50f11b70bdb43d6aa37c
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-08 09:53:26 -0600

    Added protected fields of length and width to the Rectangle class. Also added public constructor and two public methods to get the perimeter and area of the rectangle.

--- codeup-java-exercises/src/shapes/Rectangle.java
+++ codeup-java-exercises/src/shapes/Rectangle.java
@@ -1,5 +1,19 @@
 package shapes;
 
 public class Rectangle {
+    protected int length;
+    protected int width;
 
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getPerimeter(int length, int width) {
+        return (2 * length) + (2 * width);
+    }
+
+    public int getArea(int length, int width) {
+        return length * width;
+    }
 }
