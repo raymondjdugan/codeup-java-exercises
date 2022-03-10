c0c808dfba780406a83c9d3b900ec4ea26a7e710
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-10 17:01:21 -0600

    Worked on Bonus of Movies Sort application

--- codeup-java-exercises/src/arrays/movies/MoviesApplication.java
+++ codeup-java-exercises/src/arrays/movies/MoviesApplication.java
@@ -2,51 +2,73 @@ package arrays.movies;
 
 import util.Input;
 
+import java.util.Arrays;
+
 public class MoviesApplication {
     private static boolean userContinue = true;
-    private static void init(Input userInput){
+    private static final Input userInput = new Input();
+    private static Movie[] myMovies = MoviesArray.findAll();
+
+    private static void init(){
         while(userContinue) {
-            printMovies(getCategory(userChoice(userInput)));
+            printMovies(getCategory(userChoice()));
         }
     }
-    private static int userChoice(Input input) {
+
+    private static int userChoice() {
         System.out.println("What would you like to do?\n");
         System.out.println("0 - Exit");
-        System.out.println("1 - View all movies");
-        System.out.println("2 - View movies in the Animated category");
-        System.out.println("3 - View movies in the Drama category");
-        System.out.println("4 - View movies in the Horror category");
-        System.out.println("5 - View movies in the Sci-Fi category\n");
-        return input.getInt("Enter your choice");
+        System.out.println("1 - Add movie");
+        System.out.println("2 - View all movies");
+        System.out.println("3 - View movies in the Animated category");
+        System.out.println("4 - View movies in the Drama category");
+        System.out.println("5 - View movies in the Horror category");
+        System.out.println("6 - View movies in the Sci-Fi category\n");
+        return userInput.getInt(0, 6, "Enter your choice");
     }
 
     private static String getCategory(int userChoice) {
         switch (userChoice) {
             case 1:
-                return "full";
+                return "add";
             case 2:
-                return "animated";
+                return "full";
             case 3:
-                return "drama";
+                return "animated";
             case 4:
-                return "horror";
+                return "drama";
             case 5:
+                return "horror";
+            case 6:
                 return "scifi";
             default:
                 return "goodbye";
         }
     }
 
+    private static void createMovie(){
+        String name = userInput.getNextString("What is the name of the movie?");
+        String category = userInput.getNextString("What is the category of the movie?");
+
+        Movie[] newMoviesArray = Arrays.copyOf(myMovies, myMovies.length + 1);
+        newMoviesArray[newMoviesArray.length - 1] = new Movie(name, category);
+        myMovies = newMoviesArray;
+    }
+
     private static void printMovies(String userChoice) {
         if (userChoice.equals("goodbye")) {
             System.out.println("Have a nice day");
             userContinue = false;
-        } else if (userChoice.equals("full")) {
-            for (Movie movie : MoviesArray.findAll()) {
+        }
+        if (userChoice.equals("add")) {
+            createMovie();
+        }
+        if (userChoice.equals("full")) {
+            for (Movie movie : myMovies) {
                 System.out.printf("%s -- %s%n", movie.getName(), movie.getCategory());
             }
         } else {
-            for (Movie movie : MoviesArray.findAll()) {
+            for (Movie movie : myMovies) {
                 if (movie.getCategory().equals(userChoice))
                     System.out.printf("%s -- %s%n", movie.getName(), movie.getCategory());
             }
@@ -54,7 +76,8 @@ public class MoviesApplication {
     }
 
     public static void main(String[] args) {
-        Input userInput = new Input();
-        init(userInput);
+        init();
+
+        userInput.close();
     }
 }
