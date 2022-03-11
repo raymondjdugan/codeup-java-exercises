852637ff065a5c9c65d6da97da39d17c8caa7f37
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-11 09:00:12 -0600

    Refactored the getRandomNumber method to take in a param of an array length

--- codeup-java-exercises/src/arrays/ServerNameGenerator.java
+++ codeup-java-exercises/src/arrays/ServerNameGenerator.java
@@ -3,16 +3,16 @@ package arrays;
 import java.util.Random;
 
 public class ServerNameGenerator {
-    private static final String [] nouns = {"whale", "school", "steve", "dog", "pencil", "apple", "bul", "cap", "child", "city"};
+    private static final String [] nouns = {"whale", "school", "steve", "dog", "pencil", "apple", "bull", "cap", "child", "city"};
     private static final String [] adjectives = {"abounding", "boorish", "cloistered", "incompetent", "nonsensical", "parsimonious", "rambunctious", "salty", "voracious", "zany"};
 
-    private static int getRandomNumber(){
+    private static int getRandomNumber(int arrayLength){
         Random randomNumber = new Random();
-        return randomNumber.nextInt(10);
+        return randomNumber.nextInt(arrayLength);
     }
 
     public static void main(String[] args) {
         System.out.println("Here is your serer name:");
-        System.out.printf("%s-%s",nouns[getRandomNumber()], adjectives[getRandomNumber()]);
+        System.out.printf("%s-%s", adjectives[getRandomNumber(adjectives.length)],nouns[getRandomNumber(nouns.length)]);
     }
 }
